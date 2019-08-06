66a14cd8f37d3d8a9fc26ce6cefe05a960cf5d96
TylerOlli <ollityler@gmail.com>
2019-08-05 21:41:06 -0400

    Added Routes for Callback and Authentication

--- src/constants/auth.js
+++ src/constants/auth.js
@@ -1,2 +1,2 @@
-export const CLIENT_ID = '1fb0d04a94f035059b0424154fd1b18c'; // Use your client ID
+export const CLIENT_ID = 'btdoznFDqT4Ua3icyHPuK2PGHT52v93R'; // Use your client ID
 export const REDIRECT_URI = `${window.location.protocol}//${window.location.host}/callback`;
